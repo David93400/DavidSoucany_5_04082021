68d59749a0a282a825dfac61522cd2caaefe7468
Soucany <soucany.david@gmail.com>
2021-08-10 11:54:20 +0200

    dev Text confirmation order & LocalStorage

--- Frontend/assets/scripts/fonction.js
+++ Frontend/assets/scripts/fonction.js
@@ -14,18 +14,42 @@ function convertPrice(productPrice) {
   return price;
 }
 
+// Fonction pour le choix des vernis
+
+function varnishOption(product){
+const varnishChoice = document.getElementById('varnish');
+    for (let varnish of product.varnish) {
+    varnishChoice.innerHTML += `<option value="${varnish}">${varnish}</option>`;              
+    }
+    return varnish.value;
+};
+
 
 // constructeur class produit
 
-// class product {
-//   constructor(id, name, description, price, option, quantity, imgurl) {
-//     this.id = id;
-//     this.name = name;
-//     this.description = description;
-//     this.price = +price;
-//     this.option = option;
-//     this.quantity = +quantity;
-//     this.imgurl = imgurl;
-//   }
-// }
+class Product {
+  constructor(id, name, description, price, option, quantity, imgurl) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.price = +price;
+    this.option = option;
+    this.quantity = +quantity;
+    this.imgurl = imgurl;
+  }
+}
+
+function clearBtnDanger(){
+
+textConfirm.classList.add('d-none');
+textConfirm.classList.remove('btn-danger');
+varnish.classList.add('btn-light');
+varnish.classList.remove('btn-danger');
+}
+
+function clearBtnSuccess(){
+
+textConfirm.classList.add('d-none');
+textConfirm.classList.remove('btn-success');
+}
 
--- Frontend/assets/scripts/product.js
+++ Frontend/assets/scripts/product.js
@@ -1,18 +1,22 @@
 // Récupération de l'ID du produit que l'on veut afficher
 
-const SEARCHPARAMS = new URLSearchParams(window.location.search);
-const ID = SEARCHPARAMS.get('_id');
+let params = new URLSearchParams(window.location.search);
+let id = params.get('_id');
 
 const cardImg = document.getElementById('productimg');
 const cardName = document.getElementById('productname');
 const cardDescription = document.getElementById('productdescription');
 const cardPrice = document.getElementById('productprice');
+const varnish = document.getElementById('varnish');
+const quantity = document.getElementById('quantity');
+const addBtn = document.querySelector('#addCard');
 
+let textConfirm = null;
 
 
 // Modification de l'url de l'API
 
-const NEWURL = `http://localhost:3000/api/furniture/${ID}`;
+const NEWURL = `http://localhost:3000/api/furniture/${id}`;
 
 
 // Fetch de l'url correspondante au produit souhaité
@@ -33,22 +37,78 @@ fetch(NEWURL)
             cardPrice.innerHTML = convertPrice(product.price);
 
         
-        varnish(product);
-    }
-
-            // Fonction pour le choix des vernis
-
-            function varnish(product){
-            const varnishChoice = document.getElementById('varnish');
-                for (let varnish of product.varnish) {
-                varnishChoice.innerHTML += `<option value="${varnish}">${varnish}</option>`;
-                }
-            };
+        varnishOption(product);
+        }
     })
     .catch((erreur) => console.log('Erreur : ' + erreur));
+
+
+
     
-        
+
+    addBtn.addEventListener('click', addProduct)
+
+    function addProduct() {
+
+        let productAdded = {
+            name: cardName.innerHTML,
+            varnish: varnish.value,
+            price: parseFloat(cardPrice.innerHTML),
+            quantity: parseFloat(quantity.value),
+        };
+
+        // Si aucun vernis n'est sélectionné, affichage d'un message d'erreur.
+
+        if(varnish.value == 'Sélectionnez votre vernis'){
+            
+            textConfirm = document.querySelector('#text-confirmation');
+            textConfirm.innerHTML = 'Veuillez choisir votre vernis';
+            textConfirm.classList.remove('d-none');
+            textConfirm.classList.add('btn-danger');
+            varnish.classList.remove('btn-light');
+            varnish.classList.add('btn-danger');
+
+            setTimeout(clearBtnDanger, 2500);
+
+        }
+
+        let tabProducts = [];
+
+        // si LocalStorage existe, on récupere son contenu pour le mettre dans le array
+        // ensuite on le renvoie avec le/les nouveau(x) produit(s).
+
+        if(localStorage.getItem("products" !== null)) {
+
+            tabProducts = JSON.parse(localStorage.getItem("products"));
+        }
+
+        // Si le LocalStorage est vide, on le creer avec le/les produits ajouté(s)
+
+        tabProducts.push(productAdded);
+        localStorage.setItem("products", JSON.stringify(tabProducts));
+
+        // Confirmation d'ajout visuel
 
 
+        if(varnish.value !== 'Sélectionnez votre vernis' && quantity.value == 1) {
+
+            textConfirm = document.querySelector('#text-confirmation');
+            textConfirm.innerHTML = 'Votre Produit a bien été ajouté au panier';
+            textConfirm.classList.remove('d-none');
+            textConfirm.classList.add('btn-success');
+
+            setTimeout(clearBtnSuccess, 2500);
+
+        } else if ((varnish.value !== 'Sélectionnez votre vernis' && quantity.value > 1)) {
+
+            textConfirm = document.querySelector('#text-confirmation');
+            textConfirm.innerHTML = 'Vos Produits ont bien été ajoutés au panier';
+            textConfirm.classList.remove('d-none');
+            textConfirm.classList.add('btn-success');
+            quantity.value = 1;
+
+            setTimeout(clearBtnSuccess, 2500);
+        }
+    }
     
     
